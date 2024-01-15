545a9397d6167ab5e477817be2bc91a536245692
unknown <miguelferreiramota@gmail.com>
2024-01-15 15:45:29 +0100

    Update settings

--- django/core/settings.py
+++ django/core/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
