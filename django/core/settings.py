fcb5c9376ae8845bcdb46eadc09327633cba775a
unknown <miguelferreiramota@gmail.com>
2024-01-15 15:24:26 +0100

    update settings

--- django/core/settings.py
+++ django/core/settings.py
@@ -144,10 +144,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173'
-]
+CORS_ALLOWED_ORIGINS = env("ALLOWED_ORIGINS").split(" ")
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
